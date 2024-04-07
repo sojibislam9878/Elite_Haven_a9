ffea06741401a4122fef369ca4bbc36ec06ef202
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-04-07 23:07:38 +0600

    all file and folder created

---
+++ src/Pages/Root.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1 className="text-3xl">I am root</h1>
+        </div>
+    );
+};
+
+export default Root;
