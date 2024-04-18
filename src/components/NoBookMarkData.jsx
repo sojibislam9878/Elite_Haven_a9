0756296e17cdcaabb230f20edcac052964b58ae9
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-04-18 18:34:33 +0600

    a little change on react helmet

--- src/components/NoBookMarkData.jsx
+++ src/components/NoBookMarkData.jsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet";
+
 const NoBookMarkData = () => {
   return (
     <div className="bg-red-200 mt-24">
+      <Helmet>
+        <title>Elite Haven || Book Mark</title>
+      </Helmet>
       <div className="container mx-auto p-4 h-[25rem] flex items-center justify-center">
         <h1 className="font-bold text-4xl text-center text-red-700">
           There is no data
